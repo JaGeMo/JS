722906fa3d88e0081e113fcb9e624372bfd6f475
Moennig, Jan <jan.moennig@haufe-lexware.com>
2018-05-24 10:28:40 +0200

    included remains of 3/18

--- indecsion-app/public/scripts/app.js
+++ indecsion-app/public/scripts/app.js
@@ -1,57 +1,118 @@
 'use strict';
 
-var count = 0;
+console.log("app.js is running .. ");
 
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
-  // console.log("addOne", count);
-};
-var minusOne = function minusOne() {
-  count--;
-  renderCounterApp();
-  // console.log("minusOne", count);
-};
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
-  // console.log("reset", count);
+var app = {
+  title: 'title content',
+  subtitle: 'subtitle content',
+  options: []
 };
 
-// comment
-var renderCounterApp = function renderCounterApp() {
-  var templateFour = React.createElement(
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault(); // syntetic event e must be called with an preventDefault
+
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+  renderFormApp();
+  console.log(app.options);
+};
+
+var removeOptionsButton = function removeOptionsButton() {
+  app.options.length = [];
+  renderFormApp();
+};
+
+var renderFormApp = function renderFormApp() {
+
+  var templateThree = React.createElement(
     'div',
     null,
     React.createElement(
       'h1',
       null,
-      'Count: ',
-      count
+      'title: ',
+      app.title
+    ),
+    React.createElement(
+      'h2',
+      null,
+      'subtitle: ',
+      app.subtitle
+    ),
+    app.subtitle && app.options.length > 0 ? React.createElement(
+      'h2',
+      null,
+      'your options are: '
+    ) : React.createElement(
+      'h2',
+      null,
+      'no options .. '
     ),
     React.createElement(
       'button',
-      { onClick: addOne },
-      '+1'
+      { onClick: removeOptionsButton },
+      'test it'
     ),
-    React.createElement('br', null),
-    React.createElement('br', null),
     React.createElement(
-      'button',
-      { onClick: minusOne },
-      '-1'
+      'p',
+      null,
+      app.options.length
     ),
-    React.createElement('br', null),
-    React.createElement('br', null),
     React.createElement(
-      'button',
-      { onClick: reset },
-      'reset'
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'add option'
+      )
     )
   );
 
-  var testRoot4 = document.getElementById('app4');
-  ReactDOM.render(templateFour, testRoot4);
+  var testRoot3 = document.getElementById('app3');
+  ReactDOM.render(templateThree, testRoot3);
 };
 
-renderCounterApp();
+// const Items = {
+//   showItems() {
+//     return app.options.map(option => <li>app.option.value</li>);
+//   }
+// }
+
+
+renderFormApp();
+
+/*
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('app4')
+);
+*/
--- indecsion-app/src/app.js
+++ indecsion-app/src/app.js
@@ -1,22 +1,87 @@
-console.log("checks .. ");
+console.log("app.js is running .. ");
 
 let app = {
   title : 'title content',
   subtitle : 'subtitle content',
-  options : ['one', 'two', 'three']
+  options : []
 }
 
-let templateThree = (
-  <div>
-    {(app.subtitle && app.options.length > 0) ? <h2>your options are: </h2> : <h2>no options .. </h2>}
-    <h1>subtitle: {app.subtitle}</h1>
-    <ul>
-      <li>{app.options[0]}</li>
-      <li>{app.options[1]}</li>
-      <li>{app.options[2]}</li>
-    </ul>
-    </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault(); // syntetic event e must be called with an preventDefault
 
-let testRoot3 = document.getElementById('app3');
-ReactDOM.render(templateThree,testRoot3);
+  const option = e.target.elements.option.value;
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+  renderFormApp();
+  console.log(app.options);
+}
+
+const removeOptionsButton = () => {
+  app.options.length = [];
+  renderFormApp();
+}
+
+const renderFormApp = () => {
+
+  let templateThree = (
+    <div>
+      <h1>title: {app.title}</h1>
+      <h2>subtitle: {app.subtitle}</h2>
+
+      {(app.subtitle && app.options.length > 0) ? <h2>your options are: </h2> : <h2>no options .. </h2>}
+
+      <button onClick={removeOptionsButton}>test it</button>
+
+      <p>{app.options.length}</p>
+      <form onSubmit={onFormSubmit}>
+          <input type='text' name='option'/>
+          <button>add option</button>
+      </form>
+    </div>
+  );
+
+  const testRoot3 = document.getElementById('app3');
+  ReactDOM.render(templateThree,testRoot3);
+}
+
+// const Items = {
+//   showItems() {
+//     return app.options.map(option => <li>app.option.value</li>);
+//   }
+// }
+
+
+renderFormApp();
+
+/*
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('app4')
+);
+*/
