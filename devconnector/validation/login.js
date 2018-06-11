2e2dedbd02906b7d4c251d2032c6f4915c6acc27
Moennig, Jan <jan.moennig@haufe-lexware.com>
2018-06-11 08:11:18 +0200

    added final validations to login and register

--- devconnector/validation/login.js
+++ devconnector/validation/login.js
@@ -8,6 +8,18 @@ module.exports = function validateLoginInput(data) {
     errors.name = 'email must be between 2 and 30 characters';
   }
 
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
