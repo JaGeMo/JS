6ec9042f4f63fc607b32a2d96b5d9059a795b0ef
Moennig, Jan <jan.moennig@haufe-lexware.com>
2018-08-01 10:07:09 +0200

    refac fileParser

---
+++ fileParserMS/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const co = require('co');
+var prompt = require('co-prompt');
+var program = require('commander');
+var fileHandler = require('./fileHandler');
+var threshold = null;
+
+program
+  .arguments('<fileName>')
+  .option('-t, --threshold <threshold>', 'threshold')
+  .action(function(fileName) {
+    co(function *() {
+    threshold = yield prompt('threshold: ');
+    fileHandler.handleFile(fileName, threshold);
+    })
+  })
+.parse(process.argv)
+.action(() => {process.exit()})
+
+
